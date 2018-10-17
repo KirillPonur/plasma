ed2a3a30669163964fd40c9e9ab8a2a17b03a8c1
Tallahassee <aps98@mail.ru>
2018-10-18 01:43:55 +0300

    Auto stash before merge of "alex" and "origin/alex"

--- scripts/rec.py
+++ scripts/rec.py
@@ -11,8 +11,8 @@ plt.rc('text.latex', preamble=[r'\usepackage[utf8x]{inputenc}',
 
 plt.rc('font', family='serif')
 
-concentration = 'D://Labs//plasma//rec//concentration.txt'
-density = 'D://Labs//plasma//rec//density.txt'
+concentration = 'concentration.txt'
+density = 'density.txt'
 
 
 t, freq1 = parsing(concentration, 1, 0)
@@ -63,6 +63,7 @@ x = np.linspace(t[-1], t[0], 100)
 # plt.plot(x, f(x), 'c')
 plt.plot(t, N(omegap), 'or')
 print(t)
+<<<<<<< Updated upstream
 # plt.xlabel(r't, $\text{мc}$', fontsize='16')
 # plt.ylabel(r'N, $1/\text{см}^{3}$', fontsize='16')
 # plt.minorticks_on()
@@ -96,3 +97,39 @@ print(t)
 # ax.set_ylabel(r'N, $1/\text{см}^{3}$', fontsize='16')
 # plt.savefig('D:/Labs/plasma/fig/radial.pdf')
 # plt.show()
+=======
+print(N(omegap))
+plt.xlabel(r't, $\text{мc}$', fontsize='16')
+plt.ylabel(r'N, $1/\text{см}^{3}$', fontsize='16')
+plt.minorticks_on()
+plt.grid(which='major', linestyle='-')
+plt.grid(which='minor', linestyle=':')
+plt.savefig('D:/Labs/plasma/fig/decay.pdf')
+plt.show()
+
+
+# # # plt.subplot(212)
+x = np.linspace(omegap[0], omegap[-1], 100)
+plt.plot(x, N(x), 'c')
+
+plt.plot(omegap, N(omegap), 'ro')
+plt.xlabel(r'$\omega_p,\text{рад}/\text{с}$', fontsize='16')
+plt.ylabel(r'N, $1/\text{см}^{3}$', fontsize='16')
+plt.minorticks_on()
+plt.grid(which='major', linestyle='-')
+plt.grid(which='minor', linestyle=':')
+plt.savefig('D:/Labs/plasma/fig/concentration.pdf')
+plt.show()
+
+
+omegap = (omegares1**2 - omega0res**2)**(1 / 2)
+fig, ax = plt.subplots()
+ax.plot(r, N(omegap), 'r*')
+ax.minorticks_on()
+ax.grid(which='major', linestyle='-')
+ax.grid(which='minor', linestyle=':')
+ax.set_xlabel(r'r, $\text{см}$', fontsize='16')
+ax.set_ylabel(r'N, $1/\text{см}^{3}$', fontsize='16')
+plt.savefig('D:/Labs/plasma/fig/radial.pdf')
+plt.show()
+>>>>>>> Stashed changes
