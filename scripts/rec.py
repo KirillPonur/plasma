056ab1676f2bb2420cefe9ad1ae1841d929d8a7a
Unknown <ponur0kirill@gmail.com>
2018-10-23 21:50:39 +0300

    Запушил правки после Аскольда

--- scripts/rec.py
+++ scripts/rec.py
@@ -11,8 +11,8 @@ plt.rc('text.latex', preamble=[r'\usepackage[utf8x]{inputenc}',
 
 plt.rc('font', family='serif')
 
-concentration = 'concentration.txt'
-density = 'density.txt'
+concentration = 'D:\\Labs\\plasma\\scripts\\concentration.txt'
+density = 'D:\\Labs\\plasma\\scripts\\density.txt'
 
 
 t, freq1 = parsing(concentration, 1, 0)
@@ -53,24 +53,23 @@ print(t, N(omegap))
 
 
 def f(x):
-    a = 5*4.491 * 10**(11)
-    b = -0.9277
-    f = a * np.exp(b * x)
+    N0 = 1.47*10**12
+    tau0 = 4.4
+    f = N0 * np.exp(-x/tau0)
     return f
 
 
-x = np.linspace(t[-1], t[0], 100)
-# plt.plot(x, f(x), 'c')
+
+x = np.linspace(0, t[0], 100)
+plt.plot(x, f(x), 'c')
 plt.plot(t, N(omegap), 'or')
-print(t)
-<<<<<<< Updated upstream
-# plt.xlabel(r't, $\text{мc}$', fontsize='16')
-# plt.ylabel(r'N, $1/\text{см}^{3}$', fontsize='16')
-# plt.minorticks_on()
-# plt.grid(which='major', linestyle='-')
-# plt.grid(which='minor', linestyle=':')
-# plt.savefig('D:/Labs/plasma/fig/decay.pdf')
-# plt.show()
+plt.xlabel(r't, $\text{мc}$', fontsize='16')
+plt.ylabel(r'N, $1/\text{см}^{3}$', fontsize='16')
+plt.minorticks_on()
+plt.grid(which='major', linestyle='-')
+plt.grid(which='minor', linestyle=':')
+plt.savefig('D:/Labs/plasma/fig/decay.pdf')
+plt.show()
 
 
 # # # # plt.subplot(212)
@@ -97,39 +96,4 @@ print(t)
 # ax.set_ylabel(r'N, $1/\text{см}^{3}$', fontsize='16')
 # plt.savefig('D:/Labs/plasma/fig/radial.pdf')
 # plt.show()
-=======
-print(N(omegap))
-plt.xlabel(r't, $\text{мc}$', fontsize='16')
-plt.ylabel(r'N, $1/\text{см}^{3}$', fontsize='16')
-plt.minorticks_on()
-plt.grid(which='major', linestyle='-')
-plt.grid(which='minor', linestyle=':')
-plt.savefig('D:/Labs/plasma/fig/decay.pdf')
-plt.show()
 
-
-# # # plt.subplot(212)
-x = np.linspace(omegap[0], omegap[-1], 100)
-plt.plot(x, N(x), 'c')
-
-plt.plot(omegap, N(omegap), 'ro')
-plt.xlabel(r'$\omega_p,\text{рад}/\text{с}$', fontsize='16')
-plt.ylabel(r'N, $1/\text{см}^{3}$', fontsize='16')
-plt.minorticks_on()
-plt.grid(which='major', linestyle='-')
-plt.grid(which='minor', linestyle=':')
-plt.savefig('D:/Labs/plasma/fig/concentration.pdf')
-plt.show()
-
-
-omegap = (omegares1**2 - omega0res**2)**(1 / 2)
-fig, ax = plt.subplots()
-ax.plot(r, N(omegap), 'r*')
-ax.minorticks_on()
-ax.grid(which='major', linestyle='-')
-ax.grid(which='minor', linestyle=':')
-ax.set_xlabel(r'r, $\text{см}$', fontsize='16')
-ax.set_ylabel(r'N, $1/\text{см}^{3}$', fontsize='16')
-plt.savefig('D:/Labs/plasma/fig/radial.pdf')
-plt.show()
->>>>>>> Stashed changes
